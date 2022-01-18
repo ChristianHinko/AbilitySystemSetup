d1db53e7043421e0d3cec8a0ced56e7f382991ec
Brian2524 <b2hinkle@gmail.com>
2022-01-17 20:23:32 -0500

    Make Effect.Initialization tag so we have a way to identify what effects are meant for initialization. This allows us to for example, in UAttributeSet::PostGameplayEffectExecute() know if the effect is an initialization effect or not so we can do custom complex formulas on certain attributes in the cpp. That idea is what we are going to try to do in soon to come commits

---
+++ Source/AbilitySystemSetup/Private/Utilities/ASSNativeGameplayTags.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Utilities/ASSNativeGameplayTags.h"
+
+
+
+// Effect tags
+UE_DEFINE_GAMEPLAY_TAG(Tag_InitializationEffect,				TEXT("Effect.Initialization"))
---
+++ Source/AbilitySystemSetup/Public/Utilities/ASSNativeGameplayTags.h
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+
+// Effect tags
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Tag_InitializationEffect)
