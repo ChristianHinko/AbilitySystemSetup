6af974ad01a4623588c9eb29ef20815cd0746c64
Brian2524 <b2hinkle@gmail.com>
2022-04-06 07:09:43 -0400

    IAbilitySystemSetupInterface comment cleanup

--- Source/AbilitySystemSetup/Public/AbilitySystemSetupComponent/AbilitySystemSetupInterface.h
+++ Source/AbilitySystemSetup/Public/AbilitySystemSetupComponent/AbilitySystemSetupInterface.h
@@ -19,12 +19,9 @@ class UAbilitySystemSetupInterface : public UInterface
 /**
  * Is made to be implemented by Actors with the UAbilitySystemSetupComponent.
  * 
- * Provides events for:
- *		- Adding Attribute Sets to the ASC
- *		- Giving starting Abilities to the ASC
+ * Server events for giving starting Abilities and Attribute Sets via C++.
  * 
- * @SEE "AbilitySystemSetupInterface.cpp" for example implementations of these events!
- * 
+ * See AbilitySystemSetupInterface.cpp for example implementations of these events!
  */
 class ABILITYSYSTEMSETUP_API IAbilitySystemSetupInterface
 {
@@ -36,17 +33,14 @@ public:
 
 protected:
 	/**
-	 * The earliest place you can add Attribute Sets. Remember to use UObject::Rename() so that we can remove them on UnPossessed.
-	 * NOTE: Server only event.
-	 * NOTE: You probably do not need this event - just use the UAbilitySystemSetupComponent::StartingAttributeSets.
-	 * NOTE: See example implementation of this event in "AbilitySystemSetupInterface.cpp"
+	 * Server only event for giving starting Attribute Sets via C++.
+	 * NOTE: Remember to use UObject::Rename() so that we can remove them on UnPossessed.
+	 * NOTE: See example implementation of this event in "AbilitySystemSetupInterface.cpp".
 	 */
 	virtual void AddAttributeSets() { }
 	/**
-	 * The earliest place you can give Abilities. This is meant for giving Abilities and assigning Spec Handles to them.
-	 * NOTE: Server only event.
-	 * NOTE: See example implementation of this event in "AbilitySystemSetupInterface.cpp"
+	 * Server only event for giving starting abilities via C++.
+	 * NOTE: See example implementation of this event in "AbilitySystemSetupInterface.cpp".
 	 */
 	virtual void GiveStartingAbilities() { }
-
 };
